9fa52f044de10ff0a59b2cf37f9cfa9948fe191d
האוצר <haotzar-library@hotmail.com>
2026-04-30 09:48:03 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/styleplus-site',
+  assetPrefix: '/styleplus-site',
   images: {
     unoptimized: true,
   },
